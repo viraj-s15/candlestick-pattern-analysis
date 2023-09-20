cdc2262a7334d7300fafb66d3f917a33b8712e02
Viraj Shah <veer15102003@gmail.com>
2023-09-20 12:41:44 +0530

    feat: script to remove all nan vals wrtcol

---
+++ scripts/remove_not_matches.py
@@ -0,0 +1,36 @@
+import numpy as np 
+import pandas as pd
+from tqdm import tqdm
+
+
+# We need to remove the rows where the value of all of the candlestick patterns in false
+# Essentially, if all the values are false, means none of the patterns had matched
+import os
+
+_, _, files = next(os.walk("../data/with_patterns/"))
+file_count = len(files)
+print(f"The total number of CSV files is {file_count}")
+os.mkdir('../data/no_nan/')
+
+count = 0
+pbar = tqdm(total=1081)
+while count < 1081:
+    pbar.update(1)
+    df = pd.read_csv(f"../data/with_patterns/stock_{count}.csv")
+    cols = df.columns
+    cols = cols[5:]
+    df[cols] = df[cols].apply(lambda x: pd.factorize(x)[0])
+    # using boolean indexing
+    temp = df.copy()
+    temp = temp.iloc[:,5:]
+    mask = temp[cols] == 0
+    df_new = df.copy()
+    df_new.iloc[:,5:] = temp[~mask]
+    remove_nan = df_new.copy().iloc[:,5:]
+    remove_nan = remove_nan.dropna(how='all')
+    remove_nan = remove_nan.fillna(0)
+    df_new.iloc[:,5:] = remove_nan
+    df_new = df_new.dropna(subset=cols)
+    df_new.reset_index()
+    df_new.to_csv(f'../data/no_nan/stock_{count}.csv',index=False)
+    count += 1
